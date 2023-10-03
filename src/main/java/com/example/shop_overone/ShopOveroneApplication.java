eb75b5cdecb8015a74663858f309b25dfb251305
Кузьмич Кристина Игоревна <krytsina.kuzmich@softclub.by>
2023-10-03 16:06:03 +0300

    Новый проект создан

---
+++ src/main/java/com/example/shop_overone/ShopOveroneApplication.java
@@ -0,0 +1,13 @@
+package com.example.shop_overone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopOveroneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopOveroneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/shop_overone/ShopOveroneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shop_overone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopOveroneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
